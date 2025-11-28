01d5c5e5e54795516f1adb7a08240fd7abe3ea26
Alexandru Stan <>
2025-11-28 14:52:54 +0200

    Workaround: API for making superuser since I cannot use shell on render (paid functionality)

--- src/authapp/api.py
+++ src/authapp/api.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -10,3 +11,21 @@ class RetrieveAccessToken(APIView):
     def get(self, request):
         token = AuthToken.objects.get(user=request.user)
         return Response({"access_token": token.access_token})
+
+
+class CreateUser(APIView):
+
+    def post(self, request):
+        password = request.data.get("password")
+
+        user = User.objects.create_user(
+            username = 'dataroom',
+            email = 'dataroom@datarom.com',
+            is_superuser = True,
+            is_staff = True,
+            is_active = True,
+            password = password,
+        )
+        user.save()
+
+        return Response({"user": user})
