f89342deba34947fe36bd62209d360a8dc1571bf
Alexandru Stan <>
2025-11-28 17:59:37 +0200

    forgot to delete the path and usage of get_csrf function

--- src/authapp/urls.py
+++ src/authapp/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
 from .api import RetrieveAccessToken, CreateUser
-from .views import google_consent, google_callback, get_csrf
+from .views import google_consent, google_callback
 
 urlpatterns = [
     path('google/consent/', google_consent, name='google_consent'),
     path('google/callback/', google_callback, name='google_callback'),
     path('retrieve-access-token/', RetrieveAccessToken.as_view(), name='retrieve_access_token'),
     path('create/user', CreateUser.as_view(), name='create_user'),
-    path("get-csrf/", get_csrf, name="get_csrf"),
 ]
